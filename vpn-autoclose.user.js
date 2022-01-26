4ca14d9aeb72932142d1b23423543b838cb06b44
Ben Youngblood <bx.youngblood@gmail.com>
2022-01-26 14:30:53 -0700

    Create vpn-autoclose.user.js

---
+++ vpn-autoclose.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Auto-close AWS VPN auth tab
+// @version      0.1
+// @description  Closes the tab that the AWS VPN client opens to authenticate with JumpCloud.
+// @author       Ben Youngblood
+// @match        http://127.0.0.1:35001/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setTimeout(() => {
+        if (/Authentication details received/i.test(document.querySelector('body').innerText)) {
+            window.close()
+        }
+    }, 500);
+})();
