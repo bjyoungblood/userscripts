5bd67a7466cdd6ebfc33e832ef58c7f8a490a170
Ben Youngblood <bx.youngblood@gmail.com>
2026-01-28 10:01:16 -0700

    Retry Okta FastPass button every 500ms if not found
    Sometimes the one-second timeout wasn't long enough for the page to finish rendering.

--- okta-fastpass-auto-click.user.js
+++ okta-fastpass-auto-click.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Auto-click Okta FastPass on login screen
-// @version      0.1.0
+// @version      0.1.1
 // @description  Auto-click the "Sign in with Okta FastPass" button
 // @author       Ben Youngblood
 // @match        https://smartrent.okta.com/oauth2/v1/authorize*
@@ -11,14 +11,16 @@
 
 (function() {
     'use strict';
-    console.log('hi');
 
-    setTimeout(() => {
+    function tryToClickSignInButton() {
         let el = document.querySelector('.okta-verify-container a.button');
         if (el) {
             el.click();
         } else {
             console.error("Couldn't find the Sign in with Okta FastPass button");
+            setTimeout(tryToClickSignInButton, 500);
         }
-    }, 1000);
+    }
+
+    setTimeout(tryToClickSignInButton, 1000);
 })();
